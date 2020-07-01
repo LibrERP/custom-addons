8c4be40556ccad253076c03aedc8621369167aa2
michele <michele.trevisan@didotech.com>
2020-07-01 12:07:11 +0200

    [task_management]: initial commit

---
+++ task_management/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2020 Michele Trevisan - Didotech srl (www.didotech.com)
+
+{
+    'name': 'Task Management',
+    'version': '12.0.1.0',
+    'category': 'core',
+    'author': 'Didotech Srl',
+    'website': 'http://www.didotech.com',
+    'depends': [
+        'project',
+    ],
+
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
