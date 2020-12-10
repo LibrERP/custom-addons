3a9fdeb1c3bdbb278c039c58ee6d83050fd7e10d
andreinl <andrei.levin@didotech.com>
2020-12-10 14:22:09 +0100

    [IMP] l10n_it_fiscal_document_type_improved: Selection of the document type depends of the day of the Transportation Document

--- l10n_it_fiscal_document_type_improved/__manifest__.py
+++ l10n_it_fiscal_document_type_improved/__manifest__.py
@@ -7,7 +7,12 @@
 
     'description': """
         Improvements for module l10n_it_fiscal_document_type:
-        automatic document recognition based on DDT date
+        automatic document recognition based on DDT date:
+        if invoice is created in the same month as the Transportation Document,
+            than document type is TD24 and the date of invoice is Today
+        if TD is from previous month but we are in the first 10 days of the next month,
+            than document type is TD24 but invoice date is the last day from the previous month
+        All other cases it is TD25 and the date is Today 
     """,
 
     'author': "Powerp",
@@ -17,7 +22,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localisation/Italy',
-    'version': '0.1.0',
+    'version': '0.2.0',
 
     # any module necessary for this one to work correctly
     'depends': [
