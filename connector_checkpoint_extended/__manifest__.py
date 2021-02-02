cdc3970fdf9eaef6c656d9379107532e37803d3a
andreinl <andrei.levin@didotech.com>
2021-02-02 11:51:34 +0100

    [IMP] connector_checkpoint_extended: added function create_from_name_with_message()

--- connector_checkpoint_extended/__manifest__.py
+++ connector_checkpoint_extended/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Connector',
-    'version': '0.0.0.0',
+    'version': '0.0.1.0',
 
     # any module necessary for this one to work correctly
     'depends': [
--- connector_checkpoint_extended/models/checkpoints.py
+++ connector_checkpoint_extended/models/checkpoints.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ConnectorCheckpoint(models.Model):
     _inherit = 'connector.checkpoint'
 
     message = fields.Text(string="Message", required=False)
+
+    @api.model
+    def create_from_name_with_message(self, model_name, record_id,
+                         backend_model_name, backend_id, message):
+        model_model = self.env['ir.model']
+        model = model_model.search([('model', '=', model_name)], limit=1)
+        assert model, "The model %s does not exist" % model_name
+        backend = backend_model_name + ',' + str(backend_id)
+        return self.create({
+            'model_id': model.id,
+            'record_id': record_id,
+            'backend_id': backend,
+            'message': message
+        })
