4991a175ebcfade66b118537cb1ca97681b0b905
andreinl <andrei.levin@didotech.com>
2021-02-12 14:32:33 +0100

    [NEW] account_invoice_inversion: initial commit

---
+++ account_invoice_inversion/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2021 Andrei Levin - Didotech srl (www.didotech.com)
+
+{
+    'name': 'Account Invoice Inversion',
+    'summary': """
+    Inverse sign in Account Invoice and transform it in Credit Note""",
+    'description': """
+    Inverse sign in Account Invoice and transform it in Credit Note
+""",
+    'version': '12.0.1.0',
+    'category': 'Accounting',
+    'author': 'Didotech Srl',
+    'website': 'https://github.com/LibrERP/custom-addons',
+    'depends': [
+        'base',
+        # 'mail',
+        'account'
+    ],
+    'data': [
+        'views/account_invoice_view.xml'
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    "external_dependencies": {}
+}
