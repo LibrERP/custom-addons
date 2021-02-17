6cf736feb244d585a404d752ab6110a76934fd70
andreinl <andrei.levin@didotech.com>
2021-02-17 13:03:31 +0100

    [IMP] l10n_it_fatturapa_in_improved: added missing tax on rounding lines

--- l10n_it_fatturapa_in_improved/__manifest__.py
+++ l10n_it_fatturapa_in_improved/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Localisation/Italy',
-    'version': '0.1.0',
+    'version': '0.2.0',
     # any module necessary for this one to work correctly
     'depends': [
         'base',
