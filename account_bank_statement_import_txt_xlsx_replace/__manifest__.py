d061efb46471f4542942d9b9e32c0cac55101274
andreinl <andrei.levin@didotech.com>
2021-06-15 14:12:51 +0200

    [IMP] account_bank_statement_import_txt_xlsx_replace: added possibility to have one column for debit and another for credit

---
+++ account_bank_statement_import_txt_xlsx_replace/__manifest__.py
@@ -0,0 +1,39 @@
+# Copyright 2019 ForgeFlow, S.L.
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
+# © 2021 Didotech  (http://www.didotech.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Bank Statement Import TXT/CSV/XLSX",
+    "summary": "Import TXT/CSV or XLSX files as Bank Statements in Odoo",
+    "version": "12.0.2.0.5_a",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/bank-statement-import",
+    "author":
+        "ForgeFlow, "
+        "CorporateHub, "
+        "Odoo Community Association (OCA)",
+    "maintainers": [
+        "alexey-pelykh"
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account_bank_statement_import",
+        "multi_step_wizard",
+        "web_widget_dropdown_dynamic",
+    ],
+    "external_dependencies": {
+        "python": [
+            "xlrd",
+        ]
+    },
+    "data": [
+        "security/ir.model.access.csv",
+        "data/map_data.xml",
+        "views/account_bank_statement_import_sheet_mapping.xml",
+        "views/account_bank_statement_import.xml",
+        "views/account_journal_views.xml",
+        "wizards/account_bank_statement_import_sheet_mapping_wizard.xml",
+    ]
+}
