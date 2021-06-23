3e869d748b85d48885b446e081608c520ca2d343
andreinl <andrei.levin@didotech.com>
2021-06-23 18:50:41 +0200

    [FIX] account_bank_statement_import_paypal: Set fee value to 0 if no fee is present

---
+++ account_bank_statement_import_paypal_replace/__manifest__.py
@@ -0,0 +1,36 @@
+# Copyright 2014-2017 Akretion (http://www.akretion.com).
+# Copyright 2019 Tecnativa - Vicent Cubells
+# Copyright 2019-2020 Brainbean Apps (https://brainbeanapps.com)
+# Copyright 2020 CorporateHub (https://corporatehub.eu)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'PayPal CSV Format Bank Statements Import',
+    'summary': 'Import PayPal CSV files as Bank Statements in Odoo',
+    'version': '12.0.2.2.2_1',
+    'category': 'Accounting',
+    'website': 'https://github.com/OCA/bank-statement-import',
+    'author':
+        'Akretion, '
+        'CorporateHub, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'account_bank_statement_import',
+        'multi_step_wizard',
+        'web_widget_dropdown_dynamic',
+    ],
+    'external_dependencies': {
+        'python': [
+            'csv',
+        ]
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'data/maps.xml',
+        'views/account_bank_statement_import_paypal_mapping.xml',
+        'views/account_bank_statement_import.xml',
+        'wizards/account_bank_statement_import_paypal_mapping_wizard.xml',
+    ],
+}
---
+++ account_bank_statement_import_paypal_replace/readme/CONTRIBUTORS.rst
@@ -0,0 +1,10 @@
+* Alexis de Lattre <alexis.delattre@akretion.com>
+* Sebastien BEAU <sebastien.beau@akretion.com>
+* Tecnativa (https://www.tecnativa.com)
+
+  * Vicent Cubells <vicent.cubells@tecnativa.com>
+  * Victor M.M. Torres <victor.martin@tecnativa.com>
+
+* `CorporateHub <https://corporatehub.eu/>`__
+
+  * Alexey Pelykh <alexey.pelykh@corphub.eu>
