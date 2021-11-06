253cff8c54750ae338be5c02dce9e8da9c3a39f5
Fabio Colognesi <colognesi.p.i.fabio@gmail.com>
2021-11-06 08:03:58 +0100

    Moved from specific customization, shared with others.

---
+++ sale_tags/__manifest__.py
@@ -0,0 +1,61 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2020-2021 Didotech srl
+#    (<http://www.didotech.com/>).
+#
+#    Created on : 2021-07-05
+#    Author : Fabio Colognesi
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "sale_tags",
+
+    'summary': """
+        Tagging Sale Orders""",
+
+    'description': """
+        Adding tag to Sale Orders to manage filters on deliveries and invoices.
+        To help understanting if a Sale Order can be closed or it needs to be
+        followed up by sale team.
+    """,
+
+    'author': "Didotech srl",
+    'website': "http://www.didotech.com",
+    'category': 'Sales',
+    'version': '12.0.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'sale',
+        'sale_stock',
+    ],
+
+    # always loaded
+    'data': [
+        'data/scheduled_action.xml',
+        'views/sale_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
