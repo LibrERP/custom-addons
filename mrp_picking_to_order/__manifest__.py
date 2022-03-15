936372cfa7c7981ca871974401c315e9750bbc09
Fabio Colognesi <colognesi.p.i.fabio@gmail.com>
2022-03-15 22:50:14 +0100

    Managed partner not specified.

--- mrp_picking_to_order/__manifest__.py
+++ mrp_picking_to_order/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'MRP Stock Picking to Purchase Order',
-    'version': '12.0.0.0.1',
+    'version': '12.0.0.0.2',
     'category': 'MRP',
     "author": "Didotech SRL",
     'website': 'https://www.didotech.com',
