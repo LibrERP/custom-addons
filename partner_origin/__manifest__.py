d1db412c6e8df79ec7ed8402db4bf71ee49df320
andreinl <andrei.levin@didotech.com>
2022-04-19 18:41:50 +0200

    [NEW] partner_origin: Initial commit

---
+++ partner_origin/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "Partner Origin",
+    'summary': """
+        Information about partner origin""",
+    'author': "librERP enterprise network",
+    'website': "https://github.com/LibrERP/custom-addons",
+    'category': 'Sales',
+    'version': '12.0.0.0',
+    'depends': [
+        'base',
+        'contacts'
+    ],
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_view.xml'
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
