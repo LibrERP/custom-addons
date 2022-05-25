b7e1fa8abe778b750cd668f6cdd73b5b9fd898ee
andreinl <andrei.levin@didotech.com>
2022-05-25 11:29:21 +0200

    [IMP] task_management: Assign user only on tasks without user

--- task_management/models/inherit_project_task.py
+++ task_management/models/inherit_project_task.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# © 2022 Andrei Levin <andrei.levin@didotech.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 from odoo import models, api , _
 
@@ -9,7 +9,8 @@ class ProjectTask(models.Model):
 
     @api.multi
     def write(self, vals):
-        #stage_id changed: update user_id
-        if 'stage_id' in vals:
-            vals['user_id'] = self.env.uid
+        if len(self) == 1:
+            # stage_id changed: update user_id
+            if 'stage_id' in vals and not self.user_id:
+                vals['user_id'] = self.env.uid
         return super(ProjectTask, self).write(vals)
