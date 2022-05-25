ddc848ca3ba5a0080f30478a61a195b6ca5dc2b9
andreinl <andrei.levin@didotech.com>
2022-05-25 12:01:08 +0200

    [IMP] task_management: Assign user only on tasks without user

--- task_management/__manifest__.py
+++ task_management/__manifest__.py
@@ -1,14 +1,16 @@
 # © 2020 Michele Trevisan - Didotech srl (www.didotech.com)
+# © 2022 Andrei Levin - Didotech srl (www.didotech.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Task Management',
-    'version': '12.0.2.0',
-    'category': 'core',
-    'author': 'powERP enterprise network',
-    'website': 'https://www.powerp.it',
-    'development_status': 'Alpha',
+    'version': '12.0.3.0',
+    'category': 'project',
+    'author': 'LibrERP enterprise network',
+    'website': 'https://www.didotech.com',
     'license': 'LGPL-3',
-    'depends': ['project'],
+    'depends': [
+        'project'
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,
