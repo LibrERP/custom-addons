f38456cf593eb073f501b9ff727a1001fbe43702
andreinl <andrei.levin@didotech.com>
2022-09-15 19:09:08 +0200

    [NEW] wide_screen: Initial commit

---
+++ wide_screen/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright © 2022 Didotech SRL (<http://didotech.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    'name': 'Wide Screen',
+    'version': '12.0.0.1.0',
+    'category': "Tools",
+    'description': """Remove fixed width from views
+    """,
+    'author': 'Didotech SRL',
+    'website': 'https://www.didotech.com',
+    'license': 'AGPL-3',
+    "depends": [
+        'base',
+    ],
+    "data": [
+        'views/custom_width.xml'
+    ],
+    "demo": [],
+    "active": False,
+    "installable": True,
+    "application": True,
+}
