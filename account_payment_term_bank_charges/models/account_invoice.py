c12a7e93039f9b5c06ad9642c0f827bc362ed794
Fabio Giovannelli <fabio.giovannelli@didotech.com>
2022-10-05 11:06:17 +0200

    [IMP] account_payment_term_bank_charges: handle charges

--- account_payment_term_bank_charges/models/account_invoice.py
+++ account_payment_term_bank_charges/models/account_invoice.py
@@ -24,25 +24,20 @@ class AccountInvoice(models.Model):
 
     @api.onchange('payment_term_id')
     def _onchange_payment_term_id_extended(self):
-        # res = super()._onchange_payment_term_id()
-        payment_term_model = self.env['account.payment.term']
-        invoice_line_ids = []
-        # payment_term_charges_ids = self._payment_term_charges_ids()
-        spese_incasso = payment_term_model.browse(self.payment_term_id.id)
-        product_charges_ids = self._product_charges_ids()
-        if self.invoice_line_ids and self.payment_term_id:
-            new_invoice_line = []
-            for line in self.invoice_line_ids:
-                if line.id in product_charges_ids:
-                    continue
-                else:
-                    new_invoice_line.append(line)
+        if self.type == 'out_invoice' and self.state in ['draft']:
+
+            payment_term_model = self.env['account.payment.term']
+            spese_incasso = payment_term_model.browse(self.payment_term_id.id)
+            product_charges_ids = self._product_charges_ids()
+            if self.payment_term_id:
+                if self.invoice_line_ids:
+                    for line in self.invoice_line_ids:
+                        if line.product_id.id in product_charges_ids:
+                            self.invoice_line_ids -= line
 
             if spese_incasso and spese_incasso.spese_incasso_id:
                 vals = self._spese_incasso_vals(spese_incasso.spese_incasso_id)
-                new_invoice_line.append([0, 0, vals])
-
-            self.invoice_line_ids = new_invoice_line
+                spese = self.env['account.invoice.line'].new(vals)
 
     def _payment_term_charges_ids(self):
         records = self.env['account.payment.term'].search([('spese_incasso_id', '!=', False)])
@@ -54,6 +49,7 @@ class AccountInvoice(models.Model):
         return [product.id for product in products]
 
     def _spese_incasso_vals(self, product):
+        tax_id = product.taxes_id.id
         account_invoice_line_vals = {
             'invoice_id': self.id,
             'product_id': product.id,
@@ -62,17 +58,8 @@ class AccountInvoice(models.Model):
             'quantity': 1.0,
             'uom_id': product.uom_id.id,
             'price_unit': product.lst_price,
-            'invoice_line_tax_id': product.taxes_id,
+            'invoice_line_tax_ids': [(4, tax_id, 0)],
         }
-
-        # if account_invoice_line_vals.get('invoice_line_tax_id', False):
-        #     account_invoice_line_vals['invoice_line_tax_id'] = [
-        #         (6, False, product.taxes_id)]
-        #
-        # quantity = len(self.env['account.payment.term'].read(
-        #     cr, uid, payment_term_id, ['line_ids'], context=context)['line_ids'])
-        # account_invoice_line_vals['quantity'] = quantity
-
         return account_invoice_line_vals
 
     @api.multi
