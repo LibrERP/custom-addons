a5e221bc6007aabd2f4f2b454f451060a3bcc674
andreinl <andrei.levin@didotech.com>
2022-10-13 15:10:52 +0200

    [IMP] order_no_auto_sent: Don't move to Purchase Sent on order printing

--- order_no_auto_sent/__manifest__.py
+++ order_no_auto_sent/__manifest__.py
@@ -1,13 +1,13 @@
 # © 2022 Andrei Levin <andrei.levin@didotech.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 {
-    'name': "Sale Order no auto Quotation Sent",
+    'name': "Sale and Purchase Order no auto Quotation Sent",
     'summary': """
         Don't automatically change order state in 'Quotation Sent' after printing""",
     'author': "LibrERP enterprise network",
     'website': "https://github.com/LibrERP/custom-addons",
     'category': 'Sales',
-    'version': '12.0.0.0',
+    'version': '12.0.0.1',
     'depends': [
         'base',
         'sale',
--- order_no_auto_sent/models/purchase_order.py
+++ order_no_auto_sent/models/purchase_order.py
@@ -1,16 +1,16 @@
-# © 2021-2022 Didotech srl
+# © 2022 Didotech srl
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models, api, fields, _
 
 
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
 
     @api.multi
     def print_quotation(self):
         # Draft orders remains in draft
-        drfat_orders = self.filtered(lambda s: s.state == 'draft')
+        draft_orders = self.filtered(lambda s: s.state == 'draft')
         result = super().print_quotation()
-        drfat_orders.write({'state': 'draft'})
+        draft_orders.write({'state': 'draft'})
         return result
