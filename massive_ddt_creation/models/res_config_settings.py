d653a7341dac2ffe2cdb4c598cde8ce3dba66946
Solexplo <solarium@github.com>
2022-12-27 15:02:28 +0100

    [IMP] massive_ddt_creation: add module

---
+++ massive_ddt_creation/models/res_config_settings.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api, _
+from odoo.addons import decimal_precision as dp
+from odoo.tools import float_compare
+from odoo.exceptions import ValidationError
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    hide_lines_not_ready = fields.Boolean(
+        "Hides lines that have reserved availability equal 0",
+        config_parameter='massive_ddt_creation.hide_lines_not_ready')
+    massive_ddt_same_term = fields.Boolean(
+        "Selected massive DDTs must have same payment term",
+        config_parameter='massive_ddt_creation.massive_ddt_same_term')
---
+++ massive_ddt_creation/wizard/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import massive_ddt_creation
