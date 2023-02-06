d02b3969c1204adb5b1afe1a67d8caa60fe26a6c
andreinl <andrei.levin@didotech.com>
2023-02-06 10:15:08 +0100

    [FIX] l10n_it_fatturapa_in_ddt: Division by 0

--- l10n_it_fatturapa_in_ddt/__manifest__.py
+++ l10n_it_fatturapa_in_ddt/__manifest__.py
@@ -1,7 +1,8 @@
+# © 2022-2023 Marco Tosato - Didotech srl (www.didotech.com)
 # License OPL-1.0 or later (https://www.odoo.com/documentation/12.0/legal/licenses/licenses.html).
 {
     'name': 'E-Fattura In: DDT collegati',
-    'version': '12.0.9.7.2',
+    'version': '12.0.9.7.3',
     'category': 'Generic Modules/Accounting',
     'summary': '',
     'author': 'LibrERP enterprise network',
