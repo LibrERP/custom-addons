2a2a22511e550d3846f4ca49b708157d749a6656
Fabio Giovannelli <fabio.giovannelli@didotech.com>
2023-02-15 18:19:12 +0100

    [IMP] l10n_it_ddt_payment_term: update payment term according to sale order one

--- l10n_it_ddt_payment_term/models/stock.py
+++ l10n_it_ddt_payment_term/models/stock.py
@@ -10,7 +10,7 @@ class StockPickingCarriageCondition(models.Model):
     payment_term_id = fields.Many2one('account.payment.term', compute='_compute_payment_term', string="Termini di Pagamento", store=True)
 
     @api.multi
-    @api.depends('picking_ids', 'picking_ids.sale_id')
+    @api.depends('picking_ids', 'picking_ids.sale_id', 'picking_ids.sale_id.payment_term_id')
     def _compute_payment_term(self):
         for ddt in self:
             ddt.payment_term_id = ddt.picking_ids and ddt.picking_ids[0].sale_id and ddt.picking_ids[0].sale_id.payment_term_id.id or False
