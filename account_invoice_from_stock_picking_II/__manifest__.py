42b0d671ecacbd54edfc346d180a41d9f57184ad
andreinl <andrei.levin@didotech.com>
2023-02-17 14:05:38 +0100

    [IMP] account_invoice_from_stock_picking_II: Set journal, partner and date automatically if invoice is selected

--- account_invoice_from_stock_picking_II/__manifest__.py
+++ account_invoice_from_stock_picking_II/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Invoice From Stock Picking II',
-    'version': '12.0.2.20',
+    'version': '12.0.2.21',
     'category': 'Accounting',
     'summary': """Create Invoice from received Stock Pickings.
     Set existing invoice where new lines should be added or create a new one.
--- account_invoice_from_stock_picking_II/wizard/invoice_from_picking.py
+++ account_invoice_from_stock_picking_II/wizard/invoice_from_picking.py
@@ -126,8 +126,6 @@ class InvoiceFromPickings(models.TransientModel):
 
             origin.append(picking.name)
 
-        origin = ', '.join(origin)
-
         if not partner.property_account_receivable_id:
             raise Warning(_('No account defined for partner "%s".') % partner.name)
 
@@ -136,6 +134,11 @@ class InvoiceFromPickings(models.TransientModel):
         if moves_to_invoice:
             if self.invoice_id:
                 invoice = self.invoice_id
+                invoice_origin = invoice.origin and invoice.origin.split(',') or []
+                invoice_origin = [name.strip() for name in invoice_origin]
+                origin = invoice_origin + origin
+                origin = list(set(origin))
+                invoice.origin = ', '.join(origin)
             else:
                 addr = partner.address_get(['delivery', 'invoice'])
 
@@ -148,7 +151,7 @@ class InvoiceFromPickings(models.TransientModel):
                     'currency_id': partner.currency_id.id,
                     'fiscal_position_id': partner.property_account_position_id.id or False,
                     'payment_term_id': partner.property_supplier_payment_term_id.id,
-                    'origin': origin
+                    'origin': ', '.join(origin)
                 }
 
                 if invoice_type == 'in_invoice':
@@ -242,7 +245,12 @@ class InvoiceFromPickings(models.TransientModel):
                 for picking in self.picking_ids:
                     picking.credit_note = invoice.id
 
-            invoice.picking_ids = [(6, False, self.picking_ids.ids)]
+            if self.invoice_id:
+                picking_ids = self.invoice_id.picking_ids + self.picking_ids
+            else:
+                picking_ids = self.picking_ids
+
+            invoice.picking_ids = [(6, False, picking_ids.ids)]
 
             for picking in self.picking_ids:
                 picking.invoice_state = 'invoiced'
@@ -250,158 +258,6 @@ class InvoiceFromPickings(models.TransientModel):
             return invoice
         else:
             raise Warning(_('There are no lines that are not yet invoiced'))
-
-    # @api.multi
-    # def invoice_create_from_picking_ok(self):
-    #     invoice_line_model = self.env['account.invoice.line']
-    #     invoice_model = self.env['account.invoice']
-    #
-    #     partner = False
-    #     moves_to_invoice = False
-    #     names = []
-    #     origin = []
-    #
-    #     for count, picking in enumerate(self.picking_ids, start=1):
-    #         if count == 1 and picking.returned_by:
-    #             invoice_type = 'out_refund'
-    #         elif count == 1:
-    #             invoice_type = 'in_invoice'
-    #         elif picking.returned_by and invoice_type == 'in_invoice' or invoice_type == 'out_refund':
-    #             raise Warning(
-    #                 _("All selected transfers should be of the same type Mixing of Refunds and Invoices is not permitted"))
-    #
-    #         current_partner = picking.partner_id
-    #         if partner and partner.id != current_partner.id:
-    #             raise Warning(
-    #                 _("Selected Pickings have different Partner"))
-    #         elif not partner:
-    #             partner = current_partner
-    #
-    #         if picking.move_ids_without_package.filtered(lambda row: not row.invoiced):
-    #             moves_to_invoice = True
-    #
-    #         if picking.ddt_supplier_number:
-    #             names.append(picking.ddt_supplier_number)
-    #
-    #         origin.append(picking.name)
-    #
-    #     origin = ', '.join(origin)
-    #
-    #     if not partner.property_account_receivable_id:
-    #         raise Warning(_('No account defined for partner "%s".') % partner.name)
-    #
-    #     name = ', '.join(names)
-    #
-    #     if moves_to_invoice:
-    #         addr = partner.address_get(['delivery', 'invoice'])
-    #
-    #         invoice_values = {
-    #             'name': name,
-    #             'date_invoice': self.date_invoice,
-    #             'type': invoice_type,
-    #             'journal_id': self.journal_id.id,
-    #             'partner_id': addr['invoice'] or partner.id,
-    #             'currency_id': partner.currency_id.id,
-    #             'fiscal_position_id': partner.property_account_position_id.id or False,
-    #             'payment_term_id': partner.property_supplier_payment_term_id.id,
-    #             'origin': origin
-    #         }
-    #
-    #         if invoice_type == 'in_invoice':
-    #             invoice_values['account_id'] = partner.property_account_payable_id.id
-    #         elif invoice_type == 'out_refund':
-    #             invoice_values['account_id'] = partner.property_account_receivable_id.id
-    #             invoice_values['payment_term_id'] = picking.sale_id and picking.sale_id.payment_term_id.id or False
-    #
-    #         invoice = invoice_model.create(invoice_values)
-    #
-    #         for picking in self.picking_ids:
-    #             for move in picking.move_ids_without_package.filtered(lambda row: not row.invoiced):
-    #                 if invoice_type == 'out_refund':
-    #                     credit_account = move.product_id.property_account_income_id or move.product_id.categ_id.property_account_income_categ_id
-    #                     if not credit_account:
-    #                         msg = _('Default credit account is not set for the Product "{}".').format(move.product_id.name)
-    #                         raise Warning(msg)
-    #                 elif invoice_type == 'in_invoice':
-    #                     debit_account = move.product_id.property_account_expense_id or move.product_id.categ_id.property_account_expense_categ_id
-    #                     if not debit_account:
-    #                         msg = _('Default debit account is not set for the Product "{}".').format(move.product_id.name)
-    #                         raise Warning(msg)
-    #
-    #                 values = {
-    #                     'invoice_id': invoice.id,
-    #                     'name': move.product_id.name,
-    #                     'origin': picking.name,
-    #                     'quantity': move.quantity_done,
-    #                     'uom_id': move.product_uom.id,
-    #                     'product_id': move.product_id and move.product_id.id or False
-    #                 }
-    #                 if invoice_type == 'in_invoice':
-    #                     values['price_unit'] = move.purchase_line_id.price_unit or 0.0
-    #                     values['invoice_line_tax_ids'] = [(6, 0, move.purchase_line_id.taxes_id.ids)]
-    #                     values['discount'] = move.purchase_line_id and hasattr(move.purchase_line_id, 'discount') and move.purchase_line_id.discount or 0
-    #                     values['account_id'] = debit_account.id
-    #                 elif invoice_type == 'out_refund':
-    #                     values['price_unit'] = move.sale_line_id and move.sale_line_id.price_unit or move.product_id.lst_price
-    #                     values['invoice_line_tax_ids'] = move.sale_line_id and [(6, 0, move.sale_line_id.tax_id.ids)]
-    #                     values['discount'] = move.sale_line_id and move.sale_line_id.discount or 0
-    #                     values['account_id'] = credit_account.id
-    #
-    #                 invoice_line = invoice_line_model.create(values)
-    #
-    #                 if (invoice_type == 'in_invoice' and not move.purchase_line_id) or (invoice_type == 'out_refund' and not move.sale_line_id):
-    #                     # Update price & taxes
-    #                     invoice_line._compute_tax_id()
-    #                     invoice_line.set_price_unit()
-    #
-    #                 invoice_line._set_rc_flag(invoice)
-    #
-    #                 # move.qty_invoiced += move.quantity_done
-    #                 # if move.qty_invoiced == move.product_uom_qty:
-    #                 move.invoiced = True
-    #                 if move.purchase_line_id:  # Note di credito non hanno purchase lines
-    #                     move.purchase_line_id.invoiced = True
-    #                 # end if
-    #
-    #                 move.invoice_line_ids = [(4, invoice_line.id)]
-    #
-    #             if invoice_type == 'in_invoice':
-    #                 for line in picking.purchase_id.order_line.filtered(lambda x: x.product_id.type == 'service' and not x.invoiced):
-    #                     debit_account = line.product_id.property_account_expense_id
-    #                     if not debit_account:
-    #                         msg = _('Default debit account is not set for the Product "{}".').format(line.product_id.name)
-    #                         raise Warning(msg)
-    #
-    #                     values = {
-    #                         'invoice_id': invoice.id,
-    #                         'name': line.product_id.name,
-    #                         'origin': picking.name,
-    #                         'quantity': line.product_uom_qty,
-    #                         'uom_id': line.product_uom.id,
-    #                         'product_id': line.product_id and line.product_id.id or False,
-    #                         'price_unit': line.price_unit or 0.0,
-    #                         'invoice_line_tax_ids': [(6, 0, line.taxes_id.ids)],
-    #                         'account_id': debit_account.id
-    #                     }
-    #
-    #                     invoice_line = invoice_line_model.create(values)
-    #                     invoice_line._set_rc_flag(invoice)
-    #                     line.invoiced = True
-    #
-    #         invoice.compute_taxes()
-    #
-    #         if invoice_type == 'out_refund':
-    #             for picking in self.picking_ids:
-    #                 picking.credit_note = invoice.id
-    #
-    #         invoice.picking_ids = [(6, False, self.picking_ids.ids)]
-    #
-    #         for picking_id in self.picking_ids:
-    #             picking_id.invoice_state = 'invoiced'
-    #
-    #         return invoice
-    #     else:
-    #         raise Warning(_('There are no lines that are not yet invoiced'))
 
     @api.multi
     def create_invoice(self):
@@ -436,6 +292,13 @@ class InvoiceFromPickings(models.TransientModel):
             'views': [(form_id, 'form'), (tree_id, 'tree')],
             'type': 'ir.actions.act_window',
         }
+
+    @api.onchange('invoice_id')
+    def onchage_invoice_id(self):
+        if self.invoice_id:
+            self.partner_id = self.invoice_id.partner_id
+            self.journal_id = self.invoice_id.journal_id
+            self.date_invoice = self.invoice_id.date_invoice
 
 
 class InvoiceFromPickingLine(models.TransientModel):
