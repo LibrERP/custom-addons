f175b03267db8724d942eac12460323a2d265dd4
Fabio Colognesi <colognesi.p.i.fabio@gmail.com>
2023-03-31 09:11:02 +0200

    Added 'recursive_cancel' on manufacturing orders.

--- enhance_mrp/__manifest__.py
+++ enhance_mrp/__manifest__.py
@@ -34,7 +34,7 @@
     'author': "Didotech srl",
     'website': "http://www.didotech.com",
     'category': 'Manufacturing',
-    'version': '12.0.0.3.1',
+    'version': '12.0.0.4.0',
 
     # any module necessary for this one to work correctly
     'depends': [
