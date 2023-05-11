0bebef2eccc25ba640edf45b6d6983214291bf79
Solexplo <solarium@github.com>
2023-05-11 14:11:38 +0200

    [IMP] web_show_sum_header: Implement sum in the header even when single rows are selected

--- web_show_sum_header/__manifest__.py
+++ web_show_sum_header/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'web_show_sum_header',
-    'version': '12.0.0.1.0',
+    'version': '12.0.0.1.1',
     'category': "Tools",
     'description': """Show sum in list header
     """,
