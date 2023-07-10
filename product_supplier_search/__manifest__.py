340b2e0c5fa9d4a116a831c1885001bf8c72ba57
Solexplo <solarium@github.com>
2023-07-10 11:08:17 +0200

    [NEW] product_supplier_search: add module to search for supplier on product views

---
+++ product_supplier_search/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Supplier search in product view",
+    "version": "12.0.0.1",
+    "category": "Product",
+    "description": """
+        This module enables search by seller name on product template views
+    """,
+    "author": "Didotech Srl",
+    "depends": [
+        'product'
+    ],
+    "data": [
+        "views/product.xml"
+    ],
+    "auto_install": False,
+    "installable": True,
+}
