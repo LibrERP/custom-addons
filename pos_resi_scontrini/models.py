d1f87f1181d0ad14c6e3c2c1e3ec8880cdeda523
Marco Tosato <marco.tosato@didotech.com>
2023-07-18 11:27:36 +0200

    [FIX] pos_resi_scontrini: nuovo modulo per correggere il comportamento del PoS nella gestione degli scontrini di reso

---
+++ pos_resi_scontrini/models.py
@@ -0,0 +1,64 @@
+##############################################################################
+#
+#    Copyright (C) 2022-2023 Didotech SRL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import logging
+from datetime import timedelta
+from functools import partial
+
+import psycopg2
+import pytz
+
+from odoo import api, fields, models, tools, _
+from odoo.tools import float_is_zero
+from odoo.exceptions import UserError
+from odoo.http import request
+from odoo.addons import decimal_precision as dp
+
+_logger = logging.getLogger(__name__)
+
+
+class PosOrder(models.Model):
+    _inherit = 'pos.order'
+
+    @api.model
+    def _get_account_move_line_group_data_type_key(self, data_type, values, options={}):
+        """
+        Return a tuple which will be used as a key for grouping account
+        move lines in _create_account_move_line method.
+        :param data_type: 'product', 'tax', ....
+        :param values: account move line values
+        :return: tuple() representing the data_type key
+        """
+        if data_type == 'product_debit':
+            key = (
+                'product_debit',
+                values['partner_id'],
+                (values['product_id'], tuple(values['tax_ids'][0][2]), values['name']),
+                values['analytic_account_id'],
+                values['debit'] > 0,
+                values.get('currency_id'),
+            )
+
+            return key
+        else:
+            result = super()._get_account_move_line_group_data_type_key(data_type, values, options)
+            return result
+        # end if
+    # end _get_account_move_line_group_data_type_key
+# end PosOrder
