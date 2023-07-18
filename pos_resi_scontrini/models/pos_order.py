a1d4fa760417d2f6f9cdb3b813fcb2c656fdd48e
Marco Tosato <marco.tosato@didotech.com>
2023-07-18 11:29:36 +0200

    [MOVE] pos_resi_scontrini: spostato modello PosOrder dentro package models

--- pos_resi_scontrini/models/pos_order.py
+++ pos_resi_scontrini/models/pos_order.py
@@ -18,17 +18,8 @@
 ##############################################################################
 
 import logging
-from datetime import timedelta
-from functools import partial
-
-import psycopg2
-import pytz
 
 from odoo import api, fields, models, tools, _
-from odoo.tools import float_is_zero
-from odoo.exceptions import UserError
-from odoo.http import request
-from odoo.addons import decimal_precision as dp
 
 _logger = logging.getLogger(__name__)
 
