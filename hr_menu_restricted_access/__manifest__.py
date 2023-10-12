792b881f94662cfafd3cfea8872081741690099e
andreinl <andrei.levin@didotech.com>
2023-10-12 18:16:42 +0200

    [NEW] hr_menu_restricted_access: Initial commit

---
+++ hr_menu_restricted_access/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Andrei Levin <andrei.levin@didotech.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': 'HR Restricted access',
+    'version': '12.0.0.0.0',
+    'category': 'Human Resources',
+    'summary': 'Restricted access to Employees menu',
+    'author': 'Didotech srl',
+    'website': 'https://www.didotech.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr'
+    ],
+    'data': [
+        'views/hr_view.xml'
+    ],
+    'installable': True,
+}
