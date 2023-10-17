9b9883d34e205b7f48bcd7f1abf8d195f3fa5d35
andreinl <andrei.levin@didotech.com>
2023-10-17 12:19:19 +0200

    [NEW] website_menu_restricted_access: Initial commit

---
+++ website_menu_restricted_access/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Andrei Levin <andrei.levin@didotech.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    'name': 'Website Menu Restricted access',
+    'version': '12.0.0.0.0',
+    'category': 'Website',
+    'summary': 'Restrict access to Website menu to Restricted Editor and Editor and Designer',
+    'author': 'Didotech srl',
+    'website': 'https://www.didotech.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/website_view.xml'
+    ],
+    'installable': True,
+}
