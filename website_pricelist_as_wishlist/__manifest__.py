6541c0549fd905df5fd06c24846eb91dd8c5908e
andreinl <andrei.levin@didotech.com>
2023-11-14 18:18:19 +0100

    [NEW] website_pricelist_as_wishlist: Initial commit

---
+++ website_pricelist_as_wishlist/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2023 Andrei Levin <andrei.levin@didotech.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+{
+    "name": "Website Pricelist as Wishlist",
+    "summary": "Set products in Pricelist as Wishlist products",
+    "version": "14.0.1.0.0",
+    "category": "Website",
+    "website": "https://codebeex.com",
+    "author": "Codebeex srl",
+    "license": "LGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_sale_wishlist"
+    ],
+    "data": [
+        'data/ir_cron_data.xml'
+    ],
+}
