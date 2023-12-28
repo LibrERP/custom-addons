f9929f104a1f56c78bd957ffd7514bb59b2cc0a5
andreinl <andrei.levin@didotech.com>
2023-12-28 16:46:52 +0100

    [IMP] stock_inventory_report: Initial commit

---
+++ stock_inventory_report/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Stock Inventory Costs Report',
+    'version': '12.0.0.0',
+    'category': 'Stock',
+    'summary': 'Stock Inventory Report with product costs',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'report/report_stock_inventory.xml',
+        'report/stock_report_inventory_view.xml'
+    ],
+}
