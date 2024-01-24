8036b9f2006cc4459139e5a95a590b4f7bb90a89
andreinl <andrei.levin@didotech.com>
2024-01-24 15:31:39 +0100

    [IMP] rma_stage: Added description to Kanban view

--- rma_stage/__manifest__.py
+++ rma_stage/__manifest__.py
@@ -3,7 +3,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'RMA Stage',
-    'version': '12.0.0.1',
+    'version': '12.0.0.2',
     'category': 'RMA',
     'summary': 'Module adds configurable stages to RMA',
     'author': 'Codebeex srl',
