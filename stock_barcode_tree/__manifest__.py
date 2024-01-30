7530e3b14bd4862e74f9c324591b73f982ca7596
andreinl <andrei.levin@didotech.com>
2024-01-30 16:08:53 +0100

    [NEW] stock_barcode_tree: Initial commit

---
+++ stock_barcode_tree/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Stock Location Barcode in Treeview',
+    'version': '12.0.0.0',
+    'category': 'Inventory/Inventory',
+    'summary': 'Add Barcode to Location Tree',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'stock'
+    ],
+    'data': [
+        'views/stock_location_views.xml'
+    ],
+}
