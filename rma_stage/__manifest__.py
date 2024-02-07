6cb078d83b602ca7cd83a061d3fb0c20440c10a1
andreinl <andrei.levin@didotech.com>
2024-02-07 09:45:43 +0100

    [IMP] rma_stage: Changed access rights to the model Rma Stage

--- rma_stage/__manifest__.py
+++ rma_stage/__manifest__.py
@@ -3,7 +3,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'RMA Stage',
-    'version': '12.0.0.4',
+    'version': '12.0.0.5',
     'category': 'RMA',
     'summary': 'Module adds configurable stages to RMA',
     'author': 'Codebeex srl',
