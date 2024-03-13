f4e60d4b589bb2e3c9cbbd2e317702242626c92b
andreinl <andrei.levin@didotech.com>
2024-03-13 11:33:28 +0100

    [NEW] cron_disable_all: Initial commit

---
+++ cron_disable_all/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Cron Disable All',
+    'version': '16.0.0.0',
+    'category': 'Tools',
+    'summary': 'Module adds an Action which permits to deactivate all cron records',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/ir_cron_views.xml'
+    ],
+}
