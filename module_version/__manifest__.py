2b5e87eaa0a31b9dbec1054bdf21ed9ec6aa530a
andreinl <andrei.levin@didotech.com>
2024-03-25 15:00:26 +0100

    [FIX] module_version: Odoo don't use Gtk icons anymore

--- module_version/__manifest__.py
+++ module_version/__manifest__.py
@@ -1,12 +1,13 @@
 # © 2013-2021 Andrei Levin - Didotech srl (www.didotech.com)
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# noinspection PyStatementEffect
 {
     'name': 'Modules to upgrade',
-    'version': '16.0.2.0.0',
+    'version': '16.0.2.0.1',
     'category': 'Tools',
     "author": "Didotech srl",
-    'website': 'http://www.didotech.com',
+    'website': 'https://www.codebeex.com',
     'depends': [
         'base',
     ],
