65d035e9324e532f91ae66a0b0ab052445954c24
andreinl <andrei.levin@didotech.com>
2024-05-02 17:11:59 +0200

    [NEW] contract: Initial commit

---
+++ contract_amount/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Contract Amount',
+    'version': '16.0.0.0',
+    'category': "Contract Management",
+    'summary': 'Calculate amount of the next instalment',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'contract'  # OCA/contract
+    ],
+    'data': [
+        'views/contract_views.xml'
+    ],
+}
---
+++ contract_amount/models/contract.py
@@ -0,0 +1,19 @@
+# © 2024 Andrei Levin <andrei.levin@codebeex.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import Command, api, fields, models, _, Command
+# from odoo.exceptions import UserError
+# _logger = logging.getLogger(__name__)
+
+
+class ContractContract(models.Model):
+    _inherit = "contract.contract"
+
+    amount_untaxed = fields.Monetary(string="Total", store=True, compute='_compute_amounts', tracking=4)
+
+    # @api.depends_context('lang')
+    @api.depends('contract_line_ids.price_subtotal', 'contract_line_ids.quantity', 'contract_line_ids.price_unit')
+    def _compute_amounts(self):
+        for contract in self:
+            contract_lines = contract.contract_line_ids.filtered(lambda x: not x.display_type)
+            contract.amount_untaxed = sum(contract_lines.mapped('price_subtotal'))
