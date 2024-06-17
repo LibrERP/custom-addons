59d360a0c4f74a194fd779368e8d6ecb6874e1e6
andreinl <andrei.levin@didotech.com>
2024-06-17 18:02:13 +0200

    [NEW] product_movement_direction: Initial commit

---
+++ product_movement_direction/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Product Movement Direction',
+    'version': '16.0.0.0',
+    'category': 'Stock',
+    'summary': 'Add column with +/- to show if product arrives or departuring',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'stock'
+    ],
+    'data': [
+        'views/stock_views.xml'
+    ],
+}
---
+++ product_movement_direction/models/stock.py
@@ -0,0 +1,40 @@
+# © 2024 Andrei Levin <andrei.levin@codebeex.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import _, api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    direction = fields.Char(size=2, compute='_get_direction')  # , store=True)
+
+    # @api.depends('location_id')
+    def _get_direction(self):
+        for move in self:
+            if move.location_id.usage == move.location_dest_id.usage:
+                move.direction = '<>'
+            elif move.location_dest_id.usage == 'customer':
+                move.direction = '-'
+            elif move.location_id.usage == 'supplier':
+                move.direction = '+'
+            else:
+                move.direction = ''
+
+
+class StockMoveLine(models.Model):
+    _inherit = "stock.move.line"
+
+    direction = fields.Char(size=2, compute='_get_direction')  # , store=True)
+
+    # @api.depends('location_id')
+    def _get_direction(self):
+        for move in self:
+            if move.location_id.usage == move.location_dest_id.usage:
+                move.direction = '<>'
+            elif move.location_dest_id.usage == 'customer':
+                move.direction = '-'
+            elif move.location_id.usage == 'supplier':
+                move.direction = '+'
+            else:
+                move.direction = ''
