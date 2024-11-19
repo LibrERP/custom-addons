a190e8072ca6bfde4c81628110c9a78f877a592f
Andrei Levin <andrei.levin@codebeex.com>
2024-11-19 11:47:38 +0100

    [NEW] partner_data_from_vies: Port from v. 12.0

---
+++ partner_data_from_vies/__manifest__.py
@@ -0,0 +1,25 @@
+# ©  2020-2021 Didotech srl
+# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Automatic partner creation based on VAT number',
+    'version': '17.0.1.4.1',
+    'category': 'Customer Relationship Management',
+    'summary': 'Using VIES webservice, name and address information will be fetched and added to the partner.',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'LGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': [
+            'requests',
+            'BeautifulSoup4',
+        ]
+    },
+}
