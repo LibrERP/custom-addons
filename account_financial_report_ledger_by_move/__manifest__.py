e930eaedcc4caf493ec865b53c450af868dbe28f
Andrei Levin <andrei.levin@codebeex.com>
2025-01-24 13:04:22 +0100

    [FIX] account_financial_report_ledger_by_move: Don't print report twice

--- account_financial_report_ledger_by_move/__manifest__.py
+++ account_financial_report_ledger_by_move/__manifest__.py
@@ -1,13 +1,15 @@
 # Author: Didotech s.r.l.
 # Copyright 2023 Didotech.com https://www.didotech.com
+# Copyright 2025 Codebeex srl https://www.codebeex.com
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
 {
     'name': 'Account Financial Reports General Ledger By Move',
-    'version': '12.0.1.0.2',
+    'version': '12.0.1.0.3',
     'category': 'Reporting',
     'summary': 'Financial Reports extensions',
     'author': 'Didotech s.r.l.',
-    "website": "https://www.didotech.com",
+    "website": "https://www.codebeex.com",
     'depends': [
         'account',
         'date_range',
