c5b9cee71a803d457fd12086e79f7a206f16daca
Andrei Levin <andrei.levin@codebeex.com>
2025-02-14 17:21:08 +0100

    [NEW] mail_activity_done_fix: Initial commit

---
+++ mail_activity_done_fix/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Mail Activity Done Fix',
+    'version': '16.0.0.0',
+    'category': 'Discuss',
+    'summary': 'Module solve the problem of total number of activities calculation introduced by module mail_activity_done',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'mail',
+        'mail_activity_done'  # OCA/social
+    ],
+    'data': [],
+}
