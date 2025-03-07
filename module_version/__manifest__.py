ad44cab3fad7963a24e28f4133960f89dd2978dc
Andrei Levin <andrei.levin@codebeex.com>
2025-03-07 10:45:49 +0100

    [FIX] module_version: Substituted tree with list as required by Odoo 18.0

--- module_version/__manifest__.py
+++ module_version/__manifest__.py
@@ -1,10 +1,10 @@
 # © 2013-2021 Andrei Levin - Didotech srl (www.didotech.com)
-# © 2024 Andrei Levin - Codebeex srl (www.codebeex.com)
+# © 2024-2025 Andrei Levin - Codebeex srl (www.codebeex.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # noinspection PyStatementEffect
 {
     'name': 'Modules to upgrade',
-    'version': '18.0.1.0.0',
+    'version': '18.0.1.0.1',
     'category': 'Tools',
     "author": "Didotech srl",
     'website': 'http://www.didotech.com',
