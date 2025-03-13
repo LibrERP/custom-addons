a6fb1c36394be08d91ee1dd79f560c1140992e2d
Andrei Levin <andrei.levin@codebeex.com>
2025-03-13 17:15:02 +0100

    [NEW] muk_web_wide_screnn: Initial commit

---
+++ muk_web_wide_screen/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2025 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Muk Wide Screen',
+    'version': '16.0.0.0',
+    'category': "Tools",
+    'summary': 'Make muk theme use more display area',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'muk_web_theme'
+    ],
+    'data': [
+        'views/custom_width.xml'
+    ],
+}
