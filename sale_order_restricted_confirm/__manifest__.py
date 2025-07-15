9c039fc50230f616a1e02e387352f6c7dc2b9d7d
Andrei Levin <andrei.levin@codebeex.com>
2025-07-15 15:00:56 +0200

    [NEW] sale_order_restricted_confirm: Initial commit

---
+++ sale_order_restricted_confirm/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2025 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Module name',
+    'version': '12.0.0.0',
+    'category': 'XXX',
+    'summary': 'Add summary of the module',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'sale'
+    ],
+    'data': [
+        'data/groups.xml',
+        'views/sale_views.xml'
+    ],
+}
