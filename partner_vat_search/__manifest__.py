6a3792a7cb90a312228efcaf429a2e5e36b641c6
Andrei Levin <andrei.levin@codebeex.com>
2025-10-03 15:52:36 +0200

    [NEW] partner_vat_search: Initial commit

---
+++ partner_vat_search/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2025 Andrei Levin - Codebeex srl (www.codebeex.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# noinspection PyStatementEffect
+{
+    'name': 'Contact VAT Search',
+    'version': '16.0.0.0',
+    'category': 'Sales/CRM',
+    'summary': 'Search contact by VAT',
+    'author': 'Codebeex srl',
+    'website': 'https://www.codebeex.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'contacts'
+    ],
+    'data': [
+        'views/partner_views.xml'
+    ],
+}
