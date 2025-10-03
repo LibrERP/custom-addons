d46f53f188a7c617bbc25f273754a243ed0e2f03
Solex <solexpl@gmail.com>
2025-10-03 10:05:24 +0200

    [FIX] l10n_it_partner_pos: fix layout of pos

--- l10n_it_partner_pos/__manifest__.py
+++ l10n_it_partner_pos/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "ITA - POS - Partner E-invoicing Fields",
     "summary": "Expose recipient PEC and SDI code for partners in POS loader",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Point Of Sale",
     "author": "Codebeex srl",
     "license": "AGPL-3",
@@ -12,6 +12,7 @@
     "assets": {
         "point_of_sale.assets": [
             "l10n_it_partner_pos/static/src/js/pos_partner_details_edit.js",
+            "l10n_it_partner_pos/static/src/pos.css",
             "l10n_it_partner_pos/static/src/xml/pos.xml",
         ],
     },
