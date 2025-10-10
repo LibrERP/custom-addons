c8909810d130cb270b1b6c8c19583dddbe68b503
Andrei Levin <andrei.levin@codebeex.com>
2025-10-10 11:47:32 +0200

    [NEW] l10n_it_delivery_note_fix: Initial commit

---
+++ l10n_it_delivery_note_fix/models/stock_delivery_note_line.py
@@ -0,0 +1,20 @@
+# © 2025 Andrei Levin <andrei.levin@codebeex.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, models
+
+
+class StockDeliveryNoteLine(models.Model):
+    _inherit = "stock.delivery.note.line"
+
+    @api.model
+    def _prepare_detail_lines(self, moves):
+        lines = super()._prepare_detail_lines(moves)
+
+        for line in lines:
+            move = self.env['stock.move'].browse(line['move_id'])
+            line['name'] = move.sale_line_id.name
+            # if move.move_line_ids:
+            #     line['name'] += '\n Matricola/e: n° ' + ', n° '.join(move.move_line_ids.mapped('lot_id').mapped('name'))
+
+        return lines
