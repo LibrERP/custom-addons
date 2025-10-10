d50d1bc2dc0b1640b180c6e1e1538f0853233055
Solex <solexpl@gmail.com>
2025-10-10 16:52:25 +0200

    [FIX] l10n_it_delivery_note_fix: fix error on empty note name

--- l10n_it_delivery_note_fix/__manifest__.py
+++ l10n_it_delivery_note_fix/__manifest__.py
@@ -3,7 +3,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'FIX - Documento di trasporto',
-    'version': '16.0.0.0',
+    'version': '16.0.0.1',
     'category': 'Localization/Italy',
     'summary': 'Module take the name of the product in ddt from Sale Order Line',
     'author': 'Codebeex srl',
