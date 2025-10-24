ef32ce87d35dec72e269069e3cccab74678d6ccb
Andrei Levin <andrei.levin@codebeex.com>
2025-10-24 09:48:33 +0200

    [IMP] sale_invoice_picking_autoconfirm: Added exception handling in case validation generates an error, added filtering in case we have more pickings connected to Sale Order

--- sale_invoice_picking_autoconfirm/models/sale_order.py
+++ sale_invoice_picking_autoconfirm/models/sale_order.py
@@ -1,7 +1,7 @@
 # © 2025 Andrei Levin <andrei.levin@codebeex.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import _, api, Command, fields, models
+from odoo import _, api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -9,11 +9,19 @@ class SaleOrder(models.Model):
 
     def validate_picking(self):
         for order in self:
-            for picking in order.picking_ids:
+            for picking in order.picking_ids.filtered_domain([('state', '=', 'assigned')]):
                 # check lot number is correct
                 if picking.lot_numbers_match():
-                    picking.action_set_quantities_to_reservation()
-                    picking.button_validate()
+                    try:
+                        picking.action_set_quantities_to_reservation()
+                        picking.button_validate()
+                    except Exception as e:
+                        order.message_post(
+                            body=_(f"""⚠️ <strong>Error</strong><br/>
+                                   {e.name}"""),
+                            message_type='notification',
+                            subtype_xmlid='mail.mt_comment'
+                        )
                 else:
                     order.message_post(
                         body=_("""⚠️ <strong>Lot Numbers Mismatch</strong><br/>
