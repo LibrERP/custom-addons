29f956415593291dfcbbdcedc3c103411a798269
Andrei Levin <andrei.levin@codebeex.com>
2025-11-12 17:30:40 +0100

    [IMP] auditlog_fix: Resolved problem when trying to deepcopy() a dictionary containing a recordset

--- auditlog_fix/__manifest__.py
+++ auditlog_fix/__manifest__.py
@@ -3,7 +3,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'Fix Auditlog Errors',
-    'version': '12.0.0.0',
+    'version': '12.0.0.1',
     'category': 'Tools',
     'summary': 'Module Fix Auditlog Errors',
     'author': 'Codebeex srl',
--- auditlog_fix/models/rule.py
+++ auditlog_fix/models/rule.py
@@ -1,6 +1,8 @@
 # © 2025 Andrei Levin <andrei.levin@codebeex.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
+import copy
+
 from odoo import _, api, fields, models
 from odoo.addons.auditlog.models.rule import DictDiffer
 
@@ -66,3 +68,59 @@ class AuditlogRule(models.Model):
                     log,
                     list(old_values.get(res_id, EMPTY_DICT).keys()), old_values
                 )
+
+    @api.multi
+    def _make_create(self):
+        """Instanciate a create method that log its calls."""
+        self.ensure_one()
+        log_type = self.log_type
+
+        @api.model_create_multi
+        @api.returns('self', lambda value: value.id)
+        def create_full(self, vals_list, **kwargs):
+            self = self.with_context(auditlog_disabled=True)
+            rule_model = self.env['auditlog.rule']
+            new_records = create_full.origin(self, vals_list, **kwargs)
+            # Take a snapshot of record values from the cache instead of using
+            # 'read()'. It avoids issues with related/computed fields which
+            # stored in the database only at the end of the transaction, but
+            # their values exist in cache.
+            new_values = {}
+            fields_list = rule_model.get_auditlog_fields(self)
+            for new_record in new_records.sudo():
+                new_values.setdefault(new_record.id, {})
+                for fname, field in new_record._fields.items():
+                    if fname not in fields_list:
+                        continue
+                    new_values[new_record.id][fname] = field.convert_to_read(
+                        new_record[fname], new_record)
+            rule_model.sudo().create_logs(
+                self.env.uid, self._name, new_records.ids,
+                'create', None, new_values, {'log_type': log_type})
+            return new_records
+
+        def remove_recordsets(data):
+            if isinstance(data, dict):
+                return {k: remove_recordsets(v) for k, v in data.items()}
+            elif isinstance(data, list):
+                return [remove_recordsets(v) for v in data]
+            elif hasattr(data, '_name'):  # Odoo recordset
+                return None
+            return data
+
+        @api.model_create_multi
+        @api.returns('self', lambda value: value.id)
+        def create_fast(self, vals_list, **kwargs):
+            self = self.with_context(auditlog_disabled=True)
+            rule_model = self.env['auditlog.rule']
+            vals_list2 = copy.deepcopy(remove_recordsets(vals_list))
+            new_records = create_fast.origin(self, vals_list, **kwargs)
+            new_values = {}
+            for vals, new_record in zip(vals_list2, new_records):
+                new_values.setdefault(new_record.id, vals)
+            rule_model.sudo().create_logs(
+                self.env.uid, self._name, new_records.ids,
+                'create', None, new_values, {'log_type': log_type})
+            return new_records
+
+        return create_full if self.log_type == 'full' else create_fast
