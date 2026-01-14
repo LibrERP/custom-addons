a818077b3dc3ba3d2e2e7290665ba063485d7072
Andrei Levin <andrei.levin@codebeex.com>
2026-01-14 15:45:53 +0100

    [NEW] account_invoice_partner_address, sale_order_partner_address: Port from Odoo 16.0

---
+++ sale_order_partner_address/__init__.py
@@ -0,0 +1,2 @@
+# © 2022 Andrei Levin <andrei.levin@didotech.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
---
+++ sale_order_partner_address/__manifest__.py
@@ -0,0 +1,29 @@
+# © 2022-2023 Andrei Levin <andrei.levin@didotech.com>
+# © 2024-2026 Andrei Levin - Codebeex srl (www.codebeex.com)
+# Ported to Odoo 18.0 by Amirhossein Rostami
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# noinspection PyStatementEffect
+{
+    'name': "Sale Order Partner",
+    'summary': """
+        Limit Shipping and Invoice addresses to addresses that has partner as parent""",
+    'author': "Codebeex",
+    'website': "https://github.com/LibrERP/custom-addons",
+    'category': 'Sales',
+    'version': "18.0.1.0.0",
+    'depends': [
+        'base',
+        'sale'
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+    'test': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
