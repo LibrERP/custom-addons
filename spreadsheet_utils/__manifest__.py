078594246c5661c760ca5b0784f75369c4454b95
Andrei Levin <andrei.levin@codebeex.com>
2026-01-19 14:26:20 +0100

    [REF] spreadsheet: Fixed misspelling in description

--- spreadsheet_utils/__manifest__.py
+++ spreadsheet_utils/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # noinspection PyStatementEffect
 {
-    'name': 'Speadsheet',
+    'name': 'Spreadsheet',
     'version': '16.0.0.1',
     'category': 'Utility',
     'summary': 'Functions that helps to work with spreadsheet',
