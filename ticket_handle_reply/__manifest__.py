d006250736e5e224ae0cd6a96f97737a842ff55c
Andrei Levin <andrei.levin@codebeex.com>
2026-03-20 15:47:09 +0100

    [IMP] ticket_handle_reply: Disabled debug info

--- ticket_handle_reply/__manifest__.py
+++ ticket_handle_reply/__manifest__.py
@@ -3,7 +3,7 @@
 # noinspection PyStatementEffect
 {
     'name': 'Move Ticket on Reply',
-    'version': '16.0.0.0',
+    'version': '16.0.0.1',
     'category': 'Tools',
     'summary': 'On reply Move Ticket to dedicated stage',
     'author': 'Codebeex srl',
--- ticket_handle_reply/models/helpdesk_ticket.py
+++ ticket_handle_reply/models/helpdesk_ticket.py
@@ -1,7 +1,7 @@
 # © 2026 Andrei Levin <andrei.levin@codebeex.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import _, api, Command, fields, models
+from odoo import api, fields, models
 
 import logging
 logger = logging.getLogger(__name__)
@@ -10,67 +10,20 @@ logger = logging.getLogger(__name__)
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    # def message_update(self, msg_dict, update_vals=None):
-    #     logger.info('--1-- Message update')
-    #
-    #     ticket = super().message_update(msg_dict, update_vals)
-    #
-    #     # Get the actual message record
-    #     message_id = msg_dict.get('message_id')
-    #     message = self.env['mail.message'].browse(message_id) if message_id else False
-    #
-    #     if message and message.message_type == 'email':
-    #         logger.info('--2-- Type: Email')
-    #
-    #         # Skip internal users
-    #         if message.author_id and message.author_id.user_ids:
-    #             return ticket
-    #
-    #         new_stage = self._get_reopen_stage()
-    #         logger.info(f'--3-- New stage: {new_stage.name if new_stage else "None"}')
-    #
-    #         if new_stage:
-    #             logger.info("--4-- Setting New stage...")
-    #             ticket.stage_id = new_stage.id
-    #
-    #     return ticket
-
-    # def message_update(self, msg_dict, update_vals=None):
-    #     logger.info('--1-- Message update')
-    #
-    #     ticket = super().message_update(msg_dict, update_vals)
-    #
-    #     logger.info(f"{msg_dict}")
-    #
-    #     # Detect inbound email (important!)
-    #     if msg_dict.get('message_type') == 'email':
-    #         logger.info('--2-- Type: Email')
-    #         author_id = msg_dict.get('author_id')
-    #
-    #         # Optional: avoid internal users
-    #         if not self.env['res.users'].sudo().search([('partner_id', '=', author_id)]):
-    #             new_stage = self._get_reopen_stage()
-    #             logger.info(f'--3-- New stage: {new_stage.name}')
-    #             if new_stage:
-    #                 logger.info(f"--4-- Setting New stage...")
-    #                 ticket.stage_id = new_stage.id
-    #
-    #     return ticket
-
     def message_post(self, **kwargs):
-        logger.info('--1-- Message post')
+        # logger.info('--1-- Message post')
         message = super().message_post(**kwargs)
 
         if not message:
             return message
 
         if message.message_type == 'email':
-            logger.info('--2-- Type: Email')
+            # logger.info('--2-- Type: Email')
             for ticket in self:
                 stage = ticket._get_reopen_stage()
-                logger.info(f'--3-- New stage: {stage.name}')
+                # logger.info(f'--3-- New stage: {stage.name}')
                 if stage:
-                    logger.info(f"--4-- Setting New stage...")
+                    # logger.info(f"--4-- Setting New stage...")
                     ticket.stage_id = stage.id
 
         return message
