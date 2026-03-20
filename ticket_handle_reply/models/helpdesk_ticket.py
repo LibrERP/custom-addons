fec568b05960999af8479d8fd856fd43b569e8aa
Andrei Levin <andrei.levin@codebeex.com>
2026-03-20 14:51:53 +0100

    [IMP] ticket_handle_reply: Added debug info

--- ticket_handle_reply/models/helpdesk_ticket.py
+++ ticket_handle_reply/models/helpdesk_ticket.py
@@ -3,21 +3,29 @@
 
 from odoo import _, api, Command, fields, models
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     def message_update(self, msg_dict, update_vals=None):
+        logger.info('--1-- Message update')
+
         ticket = super().message_update(msg_dict, update_vals)
 
         # Detect inbound email (important!)
         if msg_dict.get('type') == 'email':
+            logger.info('--2-- Type: Email')
             author_id = msg_dict.get('author_id')
 
             # Optional: avoid internal users
             if not self.env['res.users'].sudo().search([('partner_id', '=', author_id)]):
                 new_stage = self._get_reopen_stage()
+                logger.info(f'--3-- New stage: {new_stage.name}')
                 if new_stage:
+                    logger.info(f'--4-- Setting New stage...")
                     ticket.stage_id = new_stage.id
 
         return ticket
